c83202a14b7d9a856219ba381f051147ee5e62f1
Marc-AntoineJobin <2356735@csfoy.ca>
2025-02-18 14:45:36 -0500

    first commit

---
+++ src/components/Timeline.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const periods = ["Aujourd'hui", "Cette semaine", "Ce mois"]
+
+let selectedPeriod = ref("Aujourd'hui")
+
+function selectPeriod(period: string) {
+console.log(period)
+selectedPeriod.value = period
+}
+</script>
+
+
+<template>
+    <nav>
+        <span>
+            <a href="#" v-for="period of periods" v-bind:key="period" v-on:click="selectPeriod(period)" v-bind:class="{ 'bg-primary': period === selectedPeriod}">
+                {{ period }}
+            </a>
+        </span>
+    </nav>
+    {{ selectedPeriod }}
+</template>
+
+
+<style>
+
+</style>
